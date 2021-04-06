a804c419c9ca43558699bf516cf01b212ae527bb
gzehring <greg@zehrico.com>
2021-04-06 13:17:41 -0400

    Initial UI Commit

---
+++ client/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+   <v-app>
+        <router-view></router-view>
+   </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
